f79fe9fc5c193a033f29cb4c9cd4522605ca83c8
savilaadrian <savilaarini@gmail.com>
2023-05-03 17:26:39 +0700

    menghitung kadar air

---
+++ gaita.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("ini adalah aplikasi menghitung kadar air")
+
+W1=st.number_input("masukkan nilai bobot sebelum dipanaskan")
+W2=st.number_input("masukkan nilai bobot setelah dipanaskan")
+W=st.number_input("masukkan nilai bobot sampel")
+
+tombol=st.button("hitung nilai kadar air suatu sampel")
+
+if tombol:
+    nilai_kadar_air=(W1-W2)*100/W
+    st.success(f"nilai kadar air suatu sampel adalah {nilai_kadar_air}")
