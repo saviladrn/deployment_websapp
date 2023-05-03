8f10b470e22f640a3cf6d53075fc222371380d3a
saviladrn <132325574+saviladrn@users.noreply.github.com>
2023-05-03 18:03:14 +0700

    Update gaita.py

--- gaita.py
+++ gaita.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.write("Aplikasi Menghitung Kadar Air")
 
-W1=st.number_input("masukkan nilai bobot sebelum dipanaskan")
-W2=st.number_input("masukkan nilai bobot setelah dipanaskan")
-W=st.number_input("masukkan nilai bobot sampel")
+W1=st.number_input("Masukkan nilai bobot sebelum dipanaskan")
+W2=st.number_input("Masukkan nilai bobot setelah dipanaskan")
+W=st.number_input("Masukkan nilai bobot sampel")
 
-tombol=st.button("hitung nilai kadar air suatu sampel")
+tombol=st.button("Hitung nilai kadar air suatu sampel")
 
 if tombol:
     nilai_kadar_air=(W1-W2)*100/W
